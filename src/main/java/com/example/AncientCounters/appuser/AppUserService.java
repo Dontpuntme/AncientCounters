cc76afd9890f9a1edfd79746644d2170f21e5a9a
Dontpuntme <david.martindale.99@gmail.com>
2021-09-13 10:04:19 -0400

    Setup Confirmation
    Confirmation

--- src/main/java/com/example/AncientCounters/appuser/AppUserService.java
+++ src/main/java/com/example/AncientCounters/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.AncientCounters.appuser;
 
+import com.example.AncientCounters.registration.token.ConfirmationToken;
+import com.example.AncientCounters.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +20,7 @@ public class AppUserService implements UserDetailsService {
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND,email)));
@@ -34,7 +40,20 @@ public class AppUserService implements UserDetailsService {
                 .encode(appUser.getPassword());
         appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
-//        TODO: Send confirmation token
-        return "It Works";
+
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+//        TODO: SEND EMAIL
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
